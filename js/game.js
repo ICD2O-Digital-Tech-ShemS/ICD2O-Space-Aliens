d4659241ebc1b009767fd4a7f93ebd463dd0ff76
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-20 15:13:53 +0000

    Update

--- js/game.js
+++ js/game.js
@@ -1,16 +1,26 @@
-// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
+// Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Anastasiia Lebedenko
-// Created on: May 2025
+// Created by: Kukwac
+// Created on: May 2020
 // This file contains the JS functions for index.html
-// global Phaser
+/* global Phaser */
 
+//* Game scene
 const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
-    backgroundColor: 0x5f6e7a
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    backgroundColor: 0x5f6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 }
-
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
