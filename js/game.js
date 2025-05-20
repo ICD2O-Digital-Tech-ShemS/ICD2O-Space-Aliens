d49e94eae4d2caf4c77df377221e941ff7f670f8
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-20 15:10:42 +0000

    Update 5 Assig

--- js/game.js
+++ js/game.js
@@ -1,13 +1,16 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
+//
+// Created by: Anastasiia Lebedenko
+// Created on: May 2025
 // This file contains the JS functions for index.html
-// global
+// global Phaser
+
 const config = {
-    type: phaser.AUTO,
+    type: Phaser.AUTO,
     width: 1920,
     height: 1080,
     backgroundColor: 0x5f6e7a
 }
 
-const game = new phaser.Game(config);
-console.log(game);
+const game = new Phaser.Game(config)
+console.log(game)
