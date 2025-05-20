c449ce9e01b54d8af1db5b12c6a0fe69733fe1d9
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-20 15:08:48 +0000

    Update 5 Assign-0

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
-/* global phaser */
-
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+// global
 const config = {
-    type: phaser.AUTO;
-    width: 1920;
-    height: 1080;
-    backgroundColor: 0x5f6e7a;
+    type: phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: 0x5f6e7a
 }
 
 const game = new phaser.Game(config);
